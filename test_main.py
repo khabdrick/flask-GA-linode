6a63964cc88e07804e26ff5ad197f1ad5e90047d
Muhammed Ali <muhamzyali@gmail.com>
2022-05-18 19:58:26 +0100

    complete test file

--- test_main.py
+++ test_main.py
@@ -1,7 +1,9 @@
 from main import app
+from flask import Flask, json
 
 def test_main():
     response = app.test_client().get('/') # Creates a test client for this application.
 
-    assert response.status_code == 400 # assert the stutus code of the page('/') is 200
-    assert response.data == b'Flask App' # assert the return statement ton the page
+    assert response.status_code == 200 # assert the stutus code of the page('/') is 200
+    json_object = json.loads(response.data)
+    assert (json_object[0]["country"]) == "Nigeria"  # assert the country returns Nigeria
